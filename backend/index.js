048ea1712a63ee6e9dacd93ebbbd2046c40e5927
Martí <martiram8@gmail.com>
2021-05-13 12:57:12 +0200

    mongoose connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+
 const port = 8000;
+const db = "mongodb://localhost:27017/auladb";
+
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -9,3 +13,16 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
+
+// Connect to MongoDB database
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(5000, () => {
+      console.log("Server has started!");
+    });
+  })
+  .catch((err) => {
+    console.log(`db error ${err.message}`);
+    process.exit(-1);
+  });
