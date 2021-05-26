39368acbd35afc1176b9e23f5b035c997b60bc6c
Martí <martiram8@gmail.com>
2021-05-26 10:38:39 +0200

    form afegir aula funcionant, sense arreglar frontend

--- frontend/src/pages/aules/AulesPage.jsx
+++ frontend/src/pages/aules/AulesPage.jsx
@@ -1,14 +1,23 @@
 import { useEffect, useState } from 'react'
+import { Button, Input, InputCard, InputGroup } from '../../components'
 import Navbar from '../../components/navbar/Navbar'
 import Sidebar from '../../components/sidebar/Sidebar'
 import { getToken, setToken } from '../../utils'
-import { getAules } from './aulesPage.services'
+import { getAules, postAula } from './aulesPage.services'
 
 var items = []
 
 export const AulesPage = () => {
     const [isBusy, setBusy] = useState(true)
     var [data, setData] = useState()
+    const [nom, setNom] = useState('')
+
+    const handleSubmitAula = async (e) => {
+        e.preventDefault()
+        const resp = await postAula(nom)
+        window.location.reload()
+        console.log(resp)
+    }
 
     useEffect(() => {
         const omplirAules = async () => {
@@ -26,6 +35,25 @@ export const AulesPage = () => {
         <>
             <Navbar />
             {isBusy ? <Sidebar></Sidebar> : <Sidebar items={items}></Sidebar>}
+            <InputCard>
+                <form onSubmit={handleSubmitAula}>
+                    <InputGroup>
+                        <label>Nom de l'Aula</label>
+                        <Input
+                            type="text"
+                            placeholder="1r A"
+                            required
+                            value={nom}
+                            onChange={(e) => {
+                                setNom(e.target.value)
+                            }}
+                        ></Input>
+                    </InputGroup>
+                    <Button type="submit" width="25%">
+                        Afegir
+                    </Button>
+                </form>
+            </InputCard>
         </>
     )
 }
